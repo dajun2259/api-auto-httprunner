1a9fa843cbea9bd1884f304a46bf94f4803d0d5b
大君 <xin.chen@1stcs.net>
2023-02-23 16:45:09 +0800

    1

--- utils/login.py
+++ utils/login.py
@@ -8,19 +8,24 @@
 # @Description :
 import requests
 
+from common.setting import Path
+from utils.file_utils.yaml_utils import YamlUtils
+
+config = YamlUtils().read_yaml(Path.common_path + "config.yaml")
+
 
 # import urllib3
 # # 去除警告
 # urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 def login():
-    url = "https://www.wanandroid.com/user/login"
+    url = f"{config['host']}/user/login"
     headers = {
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
     }
 
     data = {
-        "username": "大君2259",
-        "password": 123456
+        "username": config['login_config']['user_name'],
+        "password": config['login_config']['user_pass']
 
     }
     r = requests.post(url=url, headers=headers, data=data, verify=False)
